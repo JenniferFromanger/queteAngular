05a1b76d3b58a249cf0d04bef3624b42a09b158a
JenniferFromanger <jennifer.mirre@yahoo.fr>
2020-08-21 16:53:52 +0200

    resolve quest 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { MenuComponent } from './menu/menu.component';
+import { SignupComponent } from './signup/signup.component';
 
+const appRoutes: Routes = [
+  { path: 'signup', component: SignupComponent },
+  { path: 'user', component: UserProfileComponent },
+  { path: '', component: UserProfileComponent },
+];
 @NgModule({
-  declarations: [AppComponent, UserProfileComponent],
-  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    UserProfileComponent,
+    MenuComponent,
+    SignupComponent,
+  ],
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent],
 })
